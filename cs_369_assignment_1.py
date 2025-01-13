14ddfd0d990b4b362264d0dd640e5ca86111d5a0
zimmerjonathan <158153541+zimmerjonathan@users.noreply.github.com>
2025-01-13 15:01:29 -0800

    Add files via upload

---
+++ cs_369_assignment_1.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import csv
+import gzip
+import time
+from datetime import datetime, timedelta
+from collections import defaultdict
+import argparse
+
+
+def most(file_name, start, end):
+    
+    time_frames = [1, 3, 6]
+    
+    results = []
+    
+    start_time = datetime.strptime(start, "%Y-%m-%d %H")
+    end_time = datetime.strptime(end, "%Y-%m-%d %H")
+    
+    if end_time <= start_time:
+        raise ValueError("End time is before start time")
+        
+    
+    for time_frame in time_frames:
+        
+        end_time = start_time + timedelta(hours=time_frame)
+        
+        color_counts = defaultdict(int)
+        pixel_counts = defaultdict(int)
+        
+        start_timer = time.perf_counter_ns()
+
+        with gzip.open(file_name, mode='rt') as file:
+            reader = csv.reader(file)
+            next(reader)
+
+
+            for row in reader:
+
+                timestamp_str = row[0]
+                try:
+                    time_stamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f UTC")
+                except:
+                    time_stamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S UTC")
+
+                if start_time <= time_stamp < end_time:
+                    color = row[2]
+                    pixel = row[3]
+
+                    color_counts[color] += 1
+                    pixel_counts[pixel] += 1
+
+
+        end_timer = time.perf_counter_ns()
+
+
+        most_placed_color = max(color_counts, key=color_counts.get)
+        most_placed_pixel = max(pixel_counts, key=pixel_counts.get)
+        execution_time = (end_timer - start_timer) / 1000000
+
+        results.append({
+            "timeframe": f"{start_time} to {end_time}",
+            "execution_time": f"{execution_time:.0f} ms",
+            "most_placed_color": most_placed_color,
+            "most_placed_pixel": most_placed_pixel
+        })
+        
+    return results
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="r/place data analysis")
+    parser.add_argument("file_name")
+    parser.add_argument("start_time")
+    parser.add_argument("end_time")
+
+    args = parser.parse_args()
+    
+    results = most(args.file_name, args.start_time, args.end_time)
+
+    print("# Week 1 Results")
+    for index, res in enumerate(results):
+        timeframe_label = f"{[1, 3, 6][index]}- Hour Timeframe"
+        print(f"## {timeframe_label}")
+        print(f"- **Timeframe:** {res['timeframe']}")
+        print(f"- **Execution Time:** {res['execution_time']}")
+        print(f"- **Most Placed Color:** {res['most_placed_color']}")
+        print(f"- **Most Placed Pixel Location:** {res['most_placed_pixel']}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
